774c97f5f90ea6b5d61a9a3e093f24d54edda108
Charlotte <charlotteschnx@gmail.com>
2025-10-10 20:57:51 +0200

    figuring out mariadb

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
-APPLICATION_NAME ?= inception
- 
-build:
-		docker build --tag srcs/${APPLICATION_NAME} .
+all:
+	docker compose -f ./srcs/docker-compose.yml up --build
+
+vol:
+	docker compose -f ./srcs/docker-compose.yml down --volumes
+
+down:
+	docker compose -f ./srcs/docker-compose.yml down
+
+re:
+	make down
+	make vol
+	make all
+
+.PHONY: all down vol re
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+/etc/init.d/mariadb start
 
-service mysql start
-
-mysql -e "CREATE DATABASE IF NOT EXISTS ${database_name};"
-mysql -e "CREATE USER '${mysql_user}'@'%' IDENTIFIED BY '${mysql_password}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${database_name}.* TO '${mysql_user}'@'%';"
-mysql -u${mysql_root_user} -p${mysql_root_password} -e "ALTER USER '${mysql_root_user}'@'localhost' IDENTIFIED BY '${mysql_root_password}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u${mysql_root_user} -p${mysql_root_password} shutdown
+/usr/bin/mariadb -u${mysql_root_user} -p${mysql_root_password} -e "CREATE DATABASE IF NOT EXISTS ${database_name};"
+/usr/bin/mariadb -u${mysql_root_user} -p${mysql_root_password} -e "CREATE USER '${mysql_user}'@'%' IDENTIFIED BY '${mysql_password}';"
+/usr/bin/mariadb -u${mysql_root_user} -p${mysql_root_password} -e "GRANT ALL PRIVILEGES ON ${database_name}.* TO '${mysql_user}'@'%';"
+/usr/bin/mariadb -u${mysql_root_user} -p${mysql_root_password} -e "ALTER USER '${mysql_root_user}'@'localhost' IDENTIFIED BY '${mysql_root_password}';"
+/usr/bin/mariadb -u${mysql_root_user} -p${mysql_root_password} -e "FLUSH PRIVILEGES;"
+/usr/bin/mariadb-admin -u${mysql_root_user} -p${mysql_root_password} shutdown
 
 exec "$@"
